d3d9420e7acc5be98b87ad658f082be4456f5c4c
abdiamn <58066670+abdiamn@users.noreply.github.com>
2021-05-31 21:17:25 +0800

    Add files via upload

---
+++ C-1.16.py
@@ -0,0 +1,3 @@
+#Meskipun tipe numerik tidak bisa diubah,
+#daftar tipe numerik bisa berubah: nilai di dalamnya bisa berubah.
+#Dengan demikian, daftar tersebut mempertahankan identitasnya sementara isinya berubah 
